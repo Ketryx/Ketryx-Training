83c05d754a2ba9d1bf5e5d5f40ec23af8cfb148d
Milan Dzenovljanovic <miland@ketryx.com>
2024-04-11 00:35:32 +0200

    Add typescript files with JSDoc

---
+++ app/patient/common.ts
@@ -0,0 +1,23 @@
+/**
+ * Gets the patient name
+ *
+ * @itemId:pat-get-name
+ * @itemFulfills:rq-1
+ * @itemTitle:"Get patient name"
+ *
+ */
+export function getPatientName() {
+  return 'John Doe';
+}
+
+/**
+ * Gets the patient address
+ *
+ * @itemId:pat-get-address
+ * @itemHasParent:sw-1
+ */
+export function getPatientAddress() {
+  return '123 Main St.';
+}
+
+
---
+++ app/provider/common.ts
@@ -0,0 +1,20 @@
+/**
+ * Returns the name of the provider
+ *
+ * @itemId:prov-get-name
+ * @itemFulfills:rq-1
+ * @itemTitle:"Get provider name"
+ */
+export function getProviderName() {
+    return 'Dr. John Doe';
+}
+
+/**
+ * Returns the provider address
+ *
+ * @itemId:prov-get-address
+ * @itemHasParent:sw-1
+ */
+export function getProviderAddress() {
+    return '456 Elm St.';
+}
