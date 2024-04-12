c35cb4682515adb8a6c611714af006e54045adec
Gabriel Pascualy <gabrielp@ketryx.com>
2024-04-11 21:32:23 -0400

    add verification tests

--- app/provider/page.test.tsx
+++ app/provider/page.test.tsx
@@ -4,7 +4,7 @@
 import { render, screen } from "@testing-library/react";
 import Page from "./page";
 
-it("Patient App: Works with Server Components", () => {
+it("Provider App: Works with Server Components", () => {
   render(<Page />);
-  expect(screen.getByRole("heading")).toHaveTextContent("Patient App");
+  expect(screen.getByRole("heading")).toHaveTextContent("Provider App");
 });
