3ccf4e87a4034176a3ca7cbc81375adb39c5df34
Gabriel Pascualy <gabrielp@ketryx.com>
2024-04-11 21:03:54 -0400

    Update common.ts

--- app/patient/common.ts
+++ app/patient/common.ts
@@ -1,22 +1,23 @@
 /**
- * Gets the patient name
- *
- * @itemId:pat-get-name
+ * When either of the following conditions is met, the system shall issue a warning notification to the user through an appropriate user interface:
+ * a) The recommended insulin dose exceeds the predefined upper or lower limits.
+ * b) The latest entered glucose reading is outdated according to the defined maximum allowable time interval.
+ * @itemId:dose-reading-warning
  * @itemFulfills:req-patient-app-ui
- * @itemTitle:"Get patient name"
+ * @itemTitle:"Insulin Dose and Glucose Reading Warning"
  *
  */
 function getPatientName() {
   return 'John Doe';
 }
 
-/**
- * Gets the patient address
- *
- * @itemId:pat-get-address
- * @itemFulfills:req-patient-app-ui
- */
-function getPatientAddress() {
-  return '123 Main St.';
-}
+// /**
+//  * Gets the patient address
+//  *
+//  * @itemId:pat-get-address
+//  * @itemFulfills:sw-1
+//  */
+// function getPatientAddress() {
+//   return '123 Main St.';
+// }
 
