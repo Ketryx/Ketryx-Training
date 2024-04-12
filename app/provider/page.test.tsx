894a0a41de6b65a1c20a9c467f16ffc8ceffba6e
Gabriel Pascualy <gabrielp@ketryx.com>
2024-04-11 22:09:31 -0400

    Update page.test.tsx

--- app/provider/page.test.tsx
+++ app/provider/page.test.tsx
@@ -4,7 +4,7 @@
 import { render, screen } from "@testing-library/react";
 import Page from "./page";
 
-it("Test feedback form @tests:KXITM4P31AVZPAP9KPVZ37RQ6XP8MFH", () => {
+it("Test feedback form @tests:KXITM5PEB1T14C087V8WSB5BTEPAQDE", () => {
   render(<Page />);
   expect(screen.getByRole("heading")).toHaveTextContent("Provider App");
 });
