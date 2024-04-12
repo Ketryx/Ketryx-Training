aaf6c7ae4c2852095f9a715c2f11a02befa41c53
Gabriel Pascualy <gabrielp@ketryx.com>
2024-04-11 21:10:34 -0400

    Update common.ts

--- app/patient/common.ts
+++ app/patient/common.ts
@@ -2,6 +2,7 @@
  * When either of the following conditions is met, the system shall issue a warning notification to the user through an appropriate user interface:
  * a) The recommended insulin dose exceeds the predefined upper or lower limits.
  * b) The latest entered glucose reading is outdated according to the defined maximum allowable time interval.
+ *
  * @itemId:dose-reading-warning
  * @itemFulfills:req-patient-app-ui
  * @itemTitle:"Insulin Dose and Glucose Reading Warning"
