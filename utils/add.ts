1f90e206df9031e16e650c0c64870e4aad928280
Jan Pöschko <jan@ketryx.com>
2024-04-16 01:35:21 +0200

    Move utils module

---
+++ utils/add.ts
@@ -0,0 +1,15 @@
+import "server-only";
+
+/**
+ * Adds two numbers.
+ *
+ * @itemTitle:"Number adder"
+ * @itemId:add-numbers
+ *
+ * @param a First number to add.
+ * @param b Second number to add.
+ * @returns Sum of the two numbers.
+ */
+export function add(a: number, b: number) {
+  return a + b;
+}
