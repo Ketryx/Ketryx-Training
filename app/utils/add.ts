9bc26c7f5c8c328932d90a912f5b9bde2900a845
Gabriel Pascualy <gabrielp@ketryx.com>
2024-04-17 16:18:01 -0400

    Update add.ts

--- app/utils/add.ts
+++ app/utils/add.ts
@@ -1,14 +1,14 @@
-import "server-only";
+// import "server-only";
 
-/**
- * Add two numbers together
- *
- * @itemId:add-numbers
- * @itemHasParent:KT-95
- *
- * @param a
- * @param b
- */
-export function add(a: number, b: number) {
-  return a + b;
-}
+// /**
+//  * Add two numbers together
+//  *
+//  * @itemId:add-numbers
+//  * @itemHasParent:KT-95
+//  *
+//  * @param a
+//  * @param b
+//  */
+// export function add(a: number, b: number) {
+//   return a + b;
+// }
